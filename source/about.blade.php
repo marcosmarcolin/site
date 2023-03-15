bd8897ee6789d80a9228b5d8f001d771d259a7c2
Marcos Marcolin <marcos@ixcsoft.com.br>
2023-03-15 08:17:44 -0300

    feat: add link in email.

--- source/about.blade.php
+++ source/about.blade.php
@@ -15,5 +15,5 @@ description: Um pouco sobre mim
 
     <p class="mb-6">Antes, nesta área(TI) desde 2009, já atuei como Webdesigner, desenvolvedor front-end e de 2014 pra cá meu foco é desenvolvimento back-end, especificamente em PHP. Estudei Licenciatura em Informática pela UTFPR, me especializei em Desenvolvimento Web pela UNOESC e atualmente curso Engenharia de Software na Estácio de Sá.</p>
 
-    <p class="mb-6">Entre em contato comigo via email marcolindev@gmail.com ou através dos meus perfis sociais: <a href="https://twitter.com/marcolindev" target="_blank">Twitter</a>, <a href="https://www.linkedin.com/in/marcosmarcolin/" target="_blank">LinkedIn</a> ou <a href="https://github.com/marcosmarcolin" target="_blank">GitHub</a>.</p>
+    <p class="mb-6">Entre em contato comigo via email <a href="mailto:marcolindev@gmail.com">marcolindev@gmail.com</a> ou através dos meus perfis sociais: <a href="https://twitter.com/marcolindev" target="_blank">Twitter</a>, <a href="https://www.linkedin.com/in/marcosmarcolin/" target="_blank">LinkedIn</a> ou <a href="https://github.com/marcosmarcolin" target="_blank">GitHub</a>.</p>
 @endsection
