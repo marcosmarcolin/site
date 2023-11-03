cfe86df83e37c5cefa4dfd0905a5befc3ed8f000
Marcos Marcolin <marcos@ixcsoft.com.br>
2023-11-03 09:52:17 -0300

    docs: update about description.

--- source/about.blade.php
+++ source/about.blade.php
@@ -11,9 +11,11 @@ description: Um pouco sobre mim
         alt="Marcos Marcolin"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">Olá! Meu nome é <strong>Marcos Marcolin</strong>, sou Desenvolvedor de Software e atuo com Pesquisa e Desenvolvimento, buscando novas tecnologias e desenvolvendo soluções.</p>
+    <p class="mb-6">Olá! Meu nome é <strong>Marcos Marcolin</strong>, sou Engenheiro de Software e atuo com pesquisa e desenvolvimento.</p>
 
-    <p class="mb-6">Antes, nesta área(TI) desde 2009, já atuei como Webdesigner, desenvolvedor front-end e de 2014 pra cá meu foco é desenvolvimento back-end, especificamente em PHP. Estudei Licenciatura em Informática pela UTFPR, me especializei em Desenvolvimento Web pela UNOESC e atualmente curso Engenharia de Software na Estácio de Sá.</p>
+    <p class="mb-6">Antes, desde 2009, já atuei como Webdesigner, desenvolvedor front-end e de 2014 pra cá meu foco é desenvolvimento back-end, especialmente PHP.</p>
+
+    <p class="mb-6">Estudei Licenciatura em Informática na UTFPR, especialização em Desenvolvimento Web pela UNOESC e atualmente curso especialização em Engenharia de Software na Estácio de Sá.</p>
 
     <p class="mb-6">Entre em contato comigo via email <a href="mailto:marcolindev@gmail.com">marcolindev@gmail.com</a> ou através dos meus perfis sociais: <a href="https://twitter.com/marcolindev" target="_blank">Twitter</a>, <a href="https://www.linkedin.com/in/marcosmarcolin/" target="_blank">LinkedIn</a> ou <a href="https://github.com/marcosmarcolin" target="_blank">GitHub</a>.</p>
 @endsection
